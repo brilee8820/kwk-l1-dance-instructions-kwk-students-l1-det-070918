48eb3e13bbb627dbf5e0494529fe0fd1f502e6e6
brilee8820 <brilee8820@gmail.com>
2018-07-10 19:05:07 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -14,18 +14,18 @@ def skip_step(starting_foot)
   puts "Briefly raise the opposite foot, then the #{starting_foot} twice more"
 end
 def bob_the_reins
-  puts "holding your arms out in front of you, straight and at chest level"
-  puts "cross your right wrist over your left and hold them together"
-  puts "lift your arms up and down in a loose bouncing movenment, in time with the beat of the song. This movenment is repeated eight times"
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts "Lift your arms up and down in a loose bouncing movenment, in time with the beat of the song. This movenment is repeated eight times"
 end
 def lasso 
-  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
-  puts "lift right arm up and point right elbow diagonally to the right"
-  puts "raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well"
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well"
 end
 def bust_a_move
-  puts "be cool"
-  puts "be snazzy!"
+  puts "Be cool"
+  puts "Be snazzy!"
 end
 def gangnam_style
   puts starting_stance
