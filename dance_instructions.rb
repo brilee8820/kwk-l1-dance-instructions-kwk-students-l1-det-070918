3780fcbc280e7032d93101bb9bb5089314f6f779
brilee8820 <brilee8820@gmail.com>
2018-07-10 19:05:26 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,8 +4,8 @@ def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end
 def base_footwork
-  puts "lift right foot" 
-  puts "return right foot to the ground"
+  puts "Lift right foot" 
+  puts "Return right foot to the ground"
   skip_step("left foot")
 end
 def skip_step(starting_foot)
